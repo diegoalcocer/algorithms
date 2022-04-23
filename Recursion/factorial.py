0a7717935f474bc11a84d03fc66280648c384fe1
Diego <alcocerd86@gmail.com>
2022-04-23 14:57:31 -0400

    Add descriptions to the factorial algorithm

--- Recursion/factorial.py
+++ Recursion/factorial.py
@@ -5,11 +5,7 @@ import questionary
     If n = 0, then n! is defined as 1 by convention. More formally, for any integer n ≥ 0
     For example, 5! = 5 · 4 · 3 · 2 · 1 = 120.
 '''
-
-def calculate_factorial():
-    number = int(questionary.text("Please enter a number to caculate it's factorial:").ask())    
-    return number, factorial(number)
-
+# This is the factorial algorithm, it uses recursion to get the result
 def factorial(number: int):
     ''' Calculate the factorial of a positive integer (number)
     '''
@@ -19,13 +15,13 @@ def factorial(number: int):
     # n! = n * (n-1)!
     res = number*factorial(number-1)
     return res
-    
-fact={0:1}
-def factorial_memoization(number):
-    if number in fact:
-        return fact[number]
-    fact[number] = number*factorial_memoization(number-1)
-    return fact[number]
+
+
+def calculate_factorial():
+    '''This function creates an interactive command line interface.
+    You can input the number to get its factorial. '''
+    number = int(questionary.text("Please enter a number to caculate it's factorial:").ask())    
+    return number, factorial(number)
 
 if __name__ == '__main__':
     result = calculate_factorial()
